aefd39bc1b2c992f44de17919228dfd03fee6e18
abubakar83 <abubakar.abs83@gmail.com>
2025-09-01 18:11:22 +0500

    styling and positioning logo in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import { MapPin } from 'lucide-react'
+import { FaCaretDown } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const location = false
   return (
     <div className="bg-white py-3 shadow-2xl">
       <div className="max-w-6xl mx-auto flex justify-between items-center">
         {/* logo section */}
-        <div className="">
+        <div className="flex gap-7 items-center">
         <Link to={"/"}><h1 className="font-bold text-3xl"><span className="text-red-500 font-serif">Z</span>aptro</h1></Link>
         <div className="flex gap-1 cursor-pointer text-gray-700 items-center">
           <MapPin className="text-red-500"/>
+          <span className="font-semibold">{location ? <div></div> : "Add Address"}</span>
+          <FaCaretDown/>
         </div>
         </div>
       </div>
