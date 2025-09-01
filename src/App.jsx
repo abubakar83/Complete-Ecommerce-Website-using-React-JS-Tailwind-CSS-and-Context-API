bbab64dbaa7829c945eb37eac836a1869c8215fa
abubakar83 <abubakar.abs83@gmail.com>
2025-09-01 17:43:33 +0500

    Add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Products from "./pages/Products"
+import About from "./pages/About"
+import Contacts from "./pages/Contacts"
+import Cart from "./pages/Cart"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div className="text-red-500 text-4xl">App</div>
+   <BrowserRouter>
+   <Navbar/>
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/products" element={<Products/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/contact" element={<Contacts/>}/>
+    <Route path="/cart" element={<Cart/>}/>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
