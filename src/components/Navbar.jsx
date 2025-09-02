0ea63a7fc3c161c8cfe455fc267f81b8f27efc20
abubakar83 <abubakar.abs83@gmail.com>
2025-09-02 19:26:50 +0500

    styling nav items with callback styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,83 @@
-import { MapPin } from 'lucide-react'
-import { FaCaretDown } from 'react-icons/fa'
-import { Link, NavLink } from 'react-router-dom'
+import { MapPin } from "lucide-react";
+import { FaCaretDown } from "react-icons/fa";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  const location = false
+  const location = false;
   return (
     <div className="bg-white py-3 shadow-2xl">
       <div className="max-w-6xl mx-auto flex justify-between items-center">
         {/* logo section */}
         <div className="flex gap-7 items-center">
-        <Link to={"/"}><h1 className="font-bold text-3xl"><span className="text-red-500 font-serif">Z</span>aptro</h1></Link>
-        <div className="flex gap-1 cursor-pointer text-gray-700 items-center">
-          <MapPin className="text-red-500"/>
-          <span className="font-semibold">{location ? <div></div> : "Add Address"}</span>
-          <FaCaretDown/>
-        </div>
+          <Link to={"/"}>
+            <h1 className="font-bold text-3xl">
+              <span className="text-red-500 font-serif">Z</span>aptro
+            </h1>
+          </Link>
+          <div className="flex gap-1 cursor-pointer text-gray-700 items-center">
+            <MapPin className="text-red-500" />
+            <span className="font-semibold">
+              {location ? <div></div> : "Add Address"}
+            </span>
+            <FaCaretDown />
+          </div>
         </div>
         {/* Menu Section */}
         <nav>
           <ul className="flex gap-7 items-center text-xl font-semibold">
-            <NavLink to={"/"} className={({isActive}) => `${isActive ? "border-b-3 border-red-500 transition-all" : "text-black"} cursor-pointer`}><li>Home</li></NavLink>
-            <NavLink to={"products"} className={({isActive}) => `${isActive ? "border-b-3 border-red-500 transition-all" : "text-black"} cursor-pointer`}><li>Products</li></NavLink>
-            <NavLink to={"about"} className={({isActive}) => `${isActive ? "border-b-3 border-red-500 transition-all" : "text-black"} cursor-pointer`}><li>About</li></NavLink>
-            <NavLink to={"contact"} className={({isActive}) => `${isActive ? "border-b-3 border-red-500 transition-all" : "text-black"} cursor-pointer`}><li>Contact</li></NavLink>
-            
-            
-            
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 border-red-500 transition-all"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Home</li>
+            </NavLink>
+            <NavLink
+              to={"products"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 border-red-500 transition-all"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Products</li>
+            </NavLink>
+            <NavLink
+              to={"about"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 border-red-500 transition-all"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>About</li>
+            </NavLink>
+            <NavLink
+              to={"contact"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 border-red-500 transition-all"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Contact</li>
+            </NavLink>
           </ul>
         </nav>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
