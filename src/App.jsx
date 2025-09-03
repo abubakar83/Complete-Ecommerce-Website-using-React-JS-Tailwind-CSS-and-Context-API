dda3b2d9861eb18e9d046e6a0644535c34eabe4d
abubakar83 <abubakar.abs83@gmail.com>
2025-09-03 19:42:16 +0500

    install axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Products from "./pages/Products"
-import About from "./pages/About"
-import Contacts from "./pages/Contacts"
-import Cart from "./pages/Cart"
-import Navbar from "./components/Navbar"
-import { useEffect } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+import { useEffect } from "react";
 
-const App = () => { 
-
+const App = () => {
   const getLocation = async () => {
-    navigator.geolocation.getCurrentPosition(pos => {
-      const {latitude, longitude} = pos.coords
-      console.log(latitude, longitude)
-    })
-  }
-  useEffect(()=> {
-    getLocation()
-  },[])  
-  
-  return (
-   <BrowserRouter>
-   <Navbar/>
-   <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/products" element={<Products/>}/>
-    <Route path="/about" element={<About/>}/>
-    <Route path="/contact" element={<Contacts/>}/>
-    <Route path="/cart" element={<Cart/>}/>
-   </Routes>
-   </BrowserRouter>
-  )
-}
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { latitude, longitude } = pos.coords;
+      console.log(latitude, longitude);
+    });
+  };
+  useEffect(() => {
+    getLocation();
+  }, []);
 
-export default App
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contacts />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
