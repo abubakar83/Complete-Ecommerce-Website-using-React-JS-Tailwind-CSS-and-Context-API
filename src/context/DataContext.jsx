161e1aeaae8d285edf1cf64bc493eed87d930ef6
abubakar83 <abubakar.abs83@gmail.com>
2025-09-08 22:38:05 +0500

    fetching data from api with axios

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,12 +1,27 @@
+import axios from "axios";
 import { createContext, useState } from "react";
 
-export const DataContext = createContext;
+export const DataContext = createContext(null);
 
-export const DataProvider = ({children}) => {
-    const [data, setData] = useState(null)
-    return (
-        <DataContext.Provider value={{data, setData}}> 
-            {children}
-        </DataContext.Provider>
-    )
-}
+export const DataProvider = ({ children }) => {
+  const [data, setData] = useState(null);
+
+  //   fetching all products from API
+  const fetchingAllProducts = async () => {
+    try {
+        // const res = await axios.get("https://fakestoreapi.in/api/products?limit=150")
+        const res = await axios.get("https://fakestoreapi.com/products")
+        console.log(res)
+        const productsData = res.data
+        // console.log(productsData)
+        setData(productsData)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <DataContext.Provider value={{ data, setData, fetchingAllProducts }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
