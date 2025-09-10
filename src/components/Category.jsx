a40278e4206d9c0baf9815944f89176001462262
abubakar83 <abubakar.abs83@gmail.com>
2025-09-10 13:02:45 +0500

    data transfer in DataContext

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,17 +2,8 @@ import { useEffect } from "react";
 import { useData } from "../context/DataContext";
 
 const Category = () => {
-  const { data, fetchingAllProducts } = useData();
+  const { data, fetchingAllProducts, categoryOnlyData } = useData();
 
-  // helper: extract unique values from given property
-  const getUniqueCategory = (data, property) => {
-    if (!Array.isArray(data)) return []; // prevent error when data is null
-    let newVal = data.map((currElem) => currElem[property]);
-    newVal = [...new Set(newVal)];
-    return newVal;
-  };
-
-  const categoryOnlyData = getUniqueCategory(data, "category");
   console.log(categoryOnlyData);
 
   useEffect(() => {
@@ -22,7 +13,7 @@ const Category = () => {
   return (
     <div className="bg-[#101829]">
       <div className="max-w-7xl mx-auto flex flex-wrap gap-3 md:gap-4 lg:gap-6 items-center justify-center md:justify-around py-6 px-4">
-        {categoryOnlyData.map((item, index) => (
+        {categoryOnlyData?.map((item, index) => (
           <div key={index}>
             <button
               className="uppercase bg-gradient-to-r from-red-500 to-purple-500 text-white px-4 py-2 rounded-md cursor-pointer text-sm md:text-base lg:text-lg hover:scale-105 transition-transform"
