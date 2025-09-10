76e06b7688e9c2b86bc4b530e799800f9abded64
abubakar83 <abubakar.abs83@gmail.com>
2025-09-10 09:38:39 +0500

    basic code for fetching data

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useData } from "../context/DataContext";
+import FilterSection from "../components/FilterSection";
+import Loading from "../assets/Loading4.webm";
 
 const Products = () => {
+  const { data, fetchingAllProducts } = useData();
+  useEffect(() => {
+    fetchingAllProducts();
+  }, []);
   return (
-    <div>Products</div>
-  )
-}
+    <div>
+      <div className="max-w-6xl mx-auto px-4 mb-10">
+        {data?.length > 0 ? (
+          <div className="flex gap-8">
+            <FilterSection />
+            <div>{}</div>
+          </div>
+        ) : (
+          <div className="flex items-center justify-center h-[400px]">
+            <video muted autoPlay loop>
+              <source src={Loading} type="video/webm" />
+            </video>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
