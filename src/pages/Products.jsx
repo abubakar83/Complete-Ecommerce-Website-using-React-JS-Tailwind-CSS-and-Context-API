a487a58c77070875d119b234efb5b063d89c17b0
abubakar83 <abubakar.abs83@gmail.com>
2025-09-11 10:44:20 +0500

    create whole product page with usestate, functoins and looping

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,28 +1,86 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useData } from "../context/DataContext";
 import FilterSection from "../components/FilterSection";
 import Loading from "../assets/Loading4.webm";
 import ProductCard from "../components/ProductCard";
+import Pagination from "../components/Pagination";
+import notfound from "../assets/notfound.json"
+import Lottie from "lottie-react";
 
 const Products = () => {
   const { data, fetchingAllProducts } = useData();
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("All");
+  const [brand, setBrand] = useState("All");
+  const [priceRange, setPriceRange] = useState([0, 5000]);
+  const [page, setPage] = useState(1);
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+    setPage(1);
+  };
+  const handleBrandChange = (e) => {
+    setBrand(e.target.value);
+    setPage(1);
+  };
+
+  const pageHandler = (selectedPage) => {
+    setPage(selectedPage);
+    window.scrollTo(0, 0);
+  };
+
+  const filteredData = data?.filter(
+    (item) =>
+      item.title.toLowerCase().includes(search.toLowerCase()) &&
+      (category === "All" || item.category === category) &&
+      item.price >= priceRange[0] &&
+      item.price <= priceRange[1]
+  );
+  const dynamicPage = Math.ceil(filteredData?.length / 8);
+
   useEffect(() => {
     fetchingAllProducts();
   }, []);
   return (
     <div>
       <div className="max-w-6xl mx-auto px-4 mb-10">
-        {data?.length > 0 ? (
-          <div className="flex gap-8">
-            <FilterSection />
-            <div className="grid grid-cols-4 gap-7 mt-10">
-              {
-                data?.map((product, index) => {
-                  return <ProductCard key={index} product={product}/>
-                })
-              }
-              </div>
-          </div>
+        {filteredData?.length > 0 ? (
+          <>
+            <div className="flex gap-8">
+              <FilterSection
+                search={search}
+                setSearch={setSearch}
+                category={category}
+                setCategory={setCategory}
+                brand={brand}
+                setBrand={setBrand}
+                priceRange={priceRange}
+                setPriceRange={setPriceRange}
+                handleCategoryChange={handleCategoryChange}
+                handleBrandChange={handleBrandChange}
+              />
+              {filteredData.length > 0 ? (
+                <div className="flex flex-col justify-center items-center">
+                  <div className="grid grid-cols-4 gap-7 mt-10">
+                    {filteredData
+                      ?.slice(page * 8 - 8, page * 8)
+                      .map((product, index) => {
+                        return <ProductCard key={index} product={product} />;
+                      })}
+                  </div>
+                  <Pagination
+                    pageHandler={pageHandler}
+                    page={page}
+                    dynamicPage={dynamicPage}
+                  />
+                </div>
+              ) : (
+                <div className="flex justify-center items-center md-h-[600px] md-w[900px] mt-10">
+                  <Lottie animationData={notfound} className="w-[500px]"/>
+                </div>
+              )}
+            </div>
+          </>
         ) : (
           <div className="flex items-center justify-center h-[400px]">
             <video muted autoPlay loop>
