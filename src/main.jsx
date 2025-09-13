1d267676dc4109a25d2bfb23edf1691c5713c239
abubakar83 <abubakar.abs83@gmail.com>
2025-09-13 12:44:17 +0500

    put ToastContainer

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { DataProvider } from "./context/DataContext.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -16,10 +19,22 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <DataProvider>
       <CartProvider>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
-    </CartProvider>   
-  </DataProvider>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <ToastContainer
+            position="bottom-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+          <App />
+        </ClerkProvider>
+      </CartProvider>
+    </DataProvider>
   </StrictMode>
 );
